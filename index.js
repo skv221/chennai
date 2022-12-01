ea93cb363a931206815f9dd8051f70bf20b45e91
skv221 <www.venkatesan.skv@gmail.com>
2022-12-01 22:56:28 +0530

    Project Completed

--- index.js
+++ index.js
@@ -12,6 +12,20 @@ function reveal() {
         reveals[i].classList.remove("active");
       }
     }
+
+    var reveals_2 = document.querySelectorAll(".reveal-2");
+  
+    for (var i = 0; i < reveals_2.length; i++) {
+      var windowWidth = window.innerHeight;
+      var elementTop = reveals_2[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowWidth - elementVisible) {
+        reveals_2[i].classList.add("active");
+      } else {
+        reveals_2[i].classList.remove("active");
+      }
+    }
   }
   
   window.addEventListener("scroll", reveal);
